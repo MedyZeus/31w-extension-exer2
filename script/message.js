3254a170eb2823d1c836e5b754700b0ac8baf5a8
Mahdi Nadir <mehdinip@gmail.com>
2022-06-23 01:13:30 -0400

    debut de l'extension de l'exercice 2

---
+++ script/message.js
@@ -0,0 +1,6 @@
+(function(){
+    let btn = document.querySelector("#mon_bouton");
+    btn.addEventListener("mousedown", function(){
+        console.log("Bravo, le script fonctionne");
+    })
+})()

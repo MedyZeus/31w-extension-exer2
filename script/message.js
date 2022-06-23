9fca95434445afd3b021b9c2abfab33bc0a14527
Mahdi Nadir <mehdinip@gmail.com>
2022-06-23 11:14:21 -0400

    changer contenu JS et CSS

--- script/message.js
+++ script/message.js
@@ -1,18 +1,23 @@
+
 (function(){
 
     let elBoite__modale;
     let elBoite__X;
+    let elBody = document.querySelector('body');
+    let elBoiteContenu;
 
     function creerBoiteModale() {
+        elBoiteContenu = document.createElement('div');
+        elBoiteContenu.classList.add('elBoiteContenu');
         elBoite__modale = document.createElement('div');
         elBoite__X = document.createElement('button');
         elBoite__X.classList.add('boite__x');
         elBoite__modale.appendChild(elBoite__X);
+        elBoite__modale.appendChild(elBoiteContenu);
         
         elBoite__X.innerHTML = "X";
-
+        elBoiteContenu.innerHTML = "3 800, rue Sherbrooke Est Montréal (Québec) H1X 2A2 | 514 254-7131";
         elBoite__modale.classList.add('boite__modale');
-        elBody = document.querySelector('body');
         elBody.appendChild(elBoite__modale);
     }
 

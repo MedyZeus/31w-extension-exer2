1cecbd366f5cfcccd1f25584b83382b832994616
Mahdi Nadir <mehdinip@gmail.com>
2022-06-23 01:26:20 -0400

    debut affichage boite modale

--- script/message.js
+++ script/message.js
@@ -1,5 +1,14 @@
 (function(){
+
     let btn = document.querySelector("#mon_bouton");
+    let elBoite__modale = document.createElement('div');
+    elBoite__modale.classList.add('boite__modale');
+    elBody = document.querySelector('body');
+    elBody.appendChild(elBoite__modale);
+
+    //document.querySelector('.boite__modale');
+    
+    
     btn.addEventListener("mousedown", function(){
         console.log("Bravo, le script fonctionne");
     })

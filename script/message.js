25ae648958c0127c809f7a165893b325fe1c6fec
Mahdi Nadir <mehdinip@gmail.com>
2022-06-23 09:45:38 -0400

    creer et animer une boite modale

--- script/message.js
+++ script/message.js
@@ -1,15 +1,33 @@
 (function(){
 
-    let btn = document.querySelector("#mon_bouton");
-    let elBoite__modale = document.createElement('div');
-    elBoite__modale.classList.add('boite__modale');
-    elBody = document.querySelector('body');
-    elBody.appendChild(elBoite__modale);
+    let elBoite__modale;
+    let elBoite__X;
 
+    function creerBoiteModale() {
+        elBoite__modale = document.createElement('div');
+        elBoite__X = document.createElement('button');
+        elBoite__X.classList.add('boite__x');
+        elBoite__modale.appendChild(elBoite__X);
+        
+        elBoite__X.innerHTML = "X";
+
+        elBoite__modale.classList.add('boite__modale');
+        elBody = document.querySelector('body');
+        elBody.appendChild(elBoite__modale);
+    }
+
+    creerBoiteModale();
+
+    let btn = document.querySelector("#mon_bouton");
     //document.querySelector('.boite__modale');
-    
     
     btn.addEventListener("mousedown", function(){
         console.log("Bravo, le script fonctionne");
+        elBoite__modale.classList.add('boite__modale__ouvrir');
     })
+
+    elBoite__X.addEventListener('mousedown', function(){
+        elBoite__modale.classList.remove('boite__modale__ouvrir');
+    })
+
 })()
